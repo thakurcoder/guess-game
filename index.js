ca84424c915634b4e84eba47559e5c1f275d3bc8
Aman Kumar Singh <69951576+thakurcoder@users.noreply.github.com>
2023-04-07 08:29:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+// let name = prompt("name:");
+
+// let temp = document.querySelector("h1").innerText;
+// document.querySelector("h1").innerText = temp + " " + name;
+
+let n = Math.round(Math.random()*101);
+console.log(n);
+var guess = 0;
+document.querySelector("button").addEventListener("click",work);
+
+
+document.querySelectorAll("button")[1].addEventListener("click",function(){
+    location.reload();
+})
+
+
+function work (){
+    let ans = document.querySelector("input").value;
+    if(ans>n) {
+
+        document.querySelector("div").innerText = "think a smaller number ";
+        guess++;
+    }
+    else if(ans<n){
+        document.querySelector("div").innerText = "think a bigger number ";
+        guess++;
+    }
+    else{
+        document.querySelector("div").innerText = "you win  ";
+        guess++;
+        document.querySelector("div").innerText = "you guess the number in "+ guess + " times";
+    }
+    
+}
